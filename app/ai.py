31782d4b96ae839d7572bed6eb772a5ae855e0be
«Im-Abyss» <enty3811@gmail.com>
2025-02-09 03:54:08 +0300

    Важные отметки для себя

--- app/ai.py
+++ app/ai.py
@@ -4,6 +4,13 @@ from config import AI_TOKEN
 
 
 async def main(content):
+
+    '''
+    Это базовая функция, при которой
+    пользователь отправляет сообщение
+    боту, а бот отвечает с помощью ИИ
+    '''
+
     api_key = AI_TOKEN
     model = "mistral-large-latest"
 
@@ -22,6 +29,13 @@ async def main(content):
     return response.choices[0].message.content
 
 async def check_error(content):
+
+    '''
+    Эта функция специнально для проверки
+    ошибок в документе, но она ещё
+    не доработана
+    '''
+
     api_key = AI_TOKEN
     model = "mistral-large-latest"
 
